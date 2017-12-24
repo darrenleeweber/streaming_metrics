9103b6177af7ec26acdf195fe4af2b59a791d907
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-12-24 14:01:34 -0800

    MedianInt - added scala checks

---
+++ src/test/scala/org/example/metrics/MedianIntCheck.scala
@@ -0,0 +1,19 @@
+package org.example.metrics
+
+import org.scalacheck.Properties
+import org.scalacheck.Prop.forAll
+
+object MedianIntCheck extends Properties("MedianInt") {
+
+  property("median:Double") = forAll { (lst: List[Int]) =>
+    val medianInt = MedianInt()
+    lst.foreach { i => medianInt.update(i) }
+    val median = medianInt.median()
+    if (lst.isEmpty) {
+      median.isNaN
+    } else {
+      lst.min <= median && median <= lst.max
+    }
+  }
+
+}
